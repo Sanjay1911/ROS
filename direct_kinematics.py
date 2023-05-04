a980a06340c5983d062eab61aa0020cd06ad2521
Sanjay1911 <sanjayyadav.tamilselvam@tuhh.de>
2023-05-04 09:24:10 +0200

    Convert to ROS

--- direct_kinematics.py
+++ direct_kinematics.py
@@ -1,16 +1,26 @@
+import rospy
+import sys
 import numpy as np
 import math
+from std_msgs.msg import Float64MultiArray
+from sensor_msgs.msg import JointState
+
+#initialise the ROS NODE
+rospy.init_node('forward kinematics',anonymous=True)
+
+#create a publisher object to publish the output
+pub = rospy.Publisher('forward_kinematics_output',Float64MultiArray, queue_size=10)
 
 #link parameters
-a1 = float(input("a1 = "))
-a2 = float(input("a2 = "))
-a3 = float(input("a3 = "))
-a4 = float(input("a4 = "))
+a1 = 10
+a2 = 10
+a3 = 5
+a4 = 10
 
 #joint variables
-d1 = float(input("x1 = "))
-T2 = float(input("T2 = "))
-T3 = float(input("T3 = "))
+d1 = 5
+T2 = 30
+T3 = -45
 
 #degree to radian
 T2=(T2/180.0)*np.pi
@@ -51,3 +61,12 @@ print(H1_2)
 H2_3 = np.matrix(H2_3)
 print("H2_3")
 print(H2_3)
+
+if __name__=='main':
+    try:
+        pub.publish(H0_1)
+        pub.publish(H1_2)
+        pub.publish(H2_3)
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
